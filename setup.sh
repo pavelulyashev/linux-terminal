7b701b3c4559e8067f75d342edf169dda29f1c72
Pavel Ulyashev <p.ulyashev@gmail.com>
2012-11-05 20:29:33 +0700

    added colorized comments, force links

--- setup.sh
+++ setup.sh
@@ -1,42 +1,54 @@
-$CONFIG_DIR=`echo $HOME/.linux-terminal/`
-
-$VIM_DIR=`echo $HOME/.vim/`
-$VIMRC=`echo $HOME/.vimrc`
-
-$ROOT=`echo /root`
+CONFIG_DIR=$HOME/.linux-terminal/
+VIM_DIR=$HOME/.vim/
+VIMRC=$HOME/.vimrc
+ROOT=/root
 
 # install git + clone .gitconfig
-apt-get install git-core vim screen mc moc grc python-setuptools python-pip 
+echo -e "\e[1;32m--> Installing packages ...\e[0m"
+sudo apt-get install git-core vim screen mc moc grc python-setuptools python-pip 
 
-easy_install Pygments
+echo -e "\e[1;32m-----> Installing pygments ...\e[0m"
+sudo easy_install Pygments
         
+echo -e "\e[1;32m-------> Cloning general configs ...\e[0m"
+rm -rf $CONFIG_DIR
 git clone git://github.com/pavelulyashev/linux-terminal.git $CONFIG_DIR
 
 # vim
-rm -r $VIM_DIR
+echo -e "\e[1;32m---------> Configuring vim ...\e[0m"
+rm -rf $VIM_DIR
 git clone git://github.com/pavelulyashev/vim.git $VIM_DIR
 echo "source $HOME/.vim/local.vim" > $VIMRC
 
 # mc
+echo -e "\e[1;32m-----------> Configuring mc ...\e[0m"
 cp $CONFIG_DIR/mc_ini $HOME/.mc/ini
-mkdir $ROOT/.mc
-ln -s $HOME/.mc/ini $ROOT/.mc/ini
+sudo mkdir $ROOT/.mc
+sudo ln -sf $HOME/.mc/ini $ROOT/.mc/ini
 
 # moc
+echo -e "\e[1;32m-------------> Configuring moc ...\e[0m"
 cp $CONFIG_DIR/moc_config $HOME/.moc/config
 
 # screen
+echo -e "\e[1;32m---------------> Configuring screen ...\e[0m"
 cp $CONFIG_DIR/.screenrc $HOME
-ln -s $HOME/.screenrc $ROOT/.screenrc
+sudo ln -sf $HOME/.screenrc $ROOT/.screenrc
 
 # git
+echo -e "\e[1;32m-----------------> Configuring git ...\e[0m"
 cp $CONFIG_DIR/.gitconfig $HOME
-ln -s $HOME/.gitconfig $ROOT/.gitconfig
+sudo ln -sf $HOME/.gitconfig $ROOT/.gitconfig
 
 # aliases
+echo -e "\e[1;32m-------------------> Configuring root bash ...\e[0m"
 cp $CONFIG_DIR/bash/.bash* $HOME
-for file in $(ls -a $HOME | grep .bash); do ln -s $HOME/$file $ROOT/$file; done
+for file in $(ls -a $CONFIG_DIR/bash | grep .bash); do sudo ln -sf $HOME/$file $ROOT/$file; done
 
 # install mysql + copy grc config
 
+echo -e "\e[1;32m---------------------> Removing temporary files ...\e[0m"
 rm -rf $CONFIG_DIR
+
+echo -e "\e[1;32m-----------------------> Done\e[0m"
+
